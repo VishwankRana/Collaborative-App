4ce067878973f7bc8990b86511ca0a1cb9934ecb
VishwankRana <142306126+VishwankRana@users.noreply.github.com>
2026-03-14 20:41:59 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-[#6211da] text-white text-3xl font-bold">
+      Tailwind v3 is working
+    </div>
+  );
+}
+
+export default App;
